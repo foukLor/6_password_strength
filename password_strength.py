21ca5770d5e08626a6c320685a0f7e744bc96cea
foukLor <vitaminotar@yandex.com>
2016-09-29 16:59:39 +0300

    fized codestyle:following pep8

--- password_strength.py
+++ password_strength.py
@@ -3,6 +3,7 @@ import getpass
 import re
 import os
 
+
 def load_dictionary(filepath):
     if not os.path.exists(filepath):
         return None
@@ -14,45 +15,52 @@ def load_dictionary(filepath):
 
 
 def contains_date_phones_nubmers(password):
-    if re.search("(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}", #phone number
-                                         password) is not None:
+    PATTERN_DD_MM_YYYY = "(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d"
+    PATTERN_MM_DD_YYYY = "(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d"
+    if re.search("(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}",   # phone number
+                password) is not None:
         return True
-    if re.search("(?:[0-9]{1,3}\.){3}[0-9]{1,3}", password) is not None: #simple ip
+    if re.search("(?:[0-9]{1,3}\.){3}[0-9]{1,3}", password) is not None:
         return True
-    if re.search("(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d", #mm.dd.yyyy
-     password) is not None:
+    if re.search(PATTERN_MM_DD_YYYY, password) is not None:
         return True
-    if re.search("(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d" #dd-mm-yyyy
-        , password) is not None:
+    if re.search(PATTERN_DD_MM_YYYY, password) is not None:
         return True
     return False
+
+
 def check_dict(password, pass_dict):
     POINT_FOR_PASSWORD = 2
     if password in pass_dict:
-            sys.exit("Use another password")
+        return 0
     else:
         return POINT_FOR_PASSWORD
-def get_password_strength(password, path_to_dictionary = None):
+
+
+def get_password_strength(password, path_to_dictionary=None):
     result_points = 0
     points = {
-        "both_case"      : 2,
-        "length_min"     : 1,
-        "length_enough"  : 2,
-        "digits"         : 1,
-        "symbols"        : 2,
-        "abbreviations"  : 1,
-        "magic_numbers"  : 2,
-        "out_magic_numb" : 1,
-        "not_in_dict"    : 2
+        "both_case":        2,
+        "length_min":       1,
+        "length_enough":    2,
+        "digits":           1,
+        "symbols":          2,
+        "abbreviations":    1,
+        "magic_numbers":    2,
+        "out_magic_numb":   1,
+        "not_in_dict":      2
     }
     if password is None:
         return 0
     if path_to_dictionary is not None:
         password_dict = load_dictionary(path_to_dictionary)
-        result_points += check_dict(password, password_dict)
+        result_checking = check_dict(password, password_dict)
+        if result_checking == 0:
+            return 0
+        result_points += result_checking
     if password != password.lower():
-        result_points +=points["both_case"]
-    if 12 > len(password) >= 8 :
+        result_points += points["both_case"]
+    if 12 > len(password) >= 8:
         result_points += points["length_min"]
     if len(password) >= 12:
         result_points += points["length_enough"]
@@ -62,7 +70,7 @@ def get_password_strength(password, path_to_dictionary = None):
         result_points += points["symbols"]
     if re.search("[A-Z]{2,4}", password) is not None:
         result_points -= points["abbreviations"]        
-    if  not contains_date_phones_nubmers(password):
+    if not contains_date_phones_nubmers(password):
         result_points += points["out_magic_numb"]
     else:
         result_points -= points["magic_numbers"]
@@ -71,6 +79,8 @@ def get_password_strength(password, path_to_dictionary = None):
 if __name__ == '__main__':
     password = getpass.getpass("Your password:\n")
     if len(sys.argv) == 2:
-        print("The strength of your password is {}".format(get_password_strength(password, sys.argv[1])))
+        print("The strength of your password is {}".format(
+                get_password_strength(password, sys.argv[1])))
     else:
-        print("The strength of your password is {}".format(get_password_strength(password)))
+        print("The strength of your password is {}".format(
+                    get_password_strength(password)))
